55f35d17abfd0e34dd3d14da518aabf2471f72d5
nonexistant <nonexistant@nonexistant.nonexistant>
2025-06-06 17:43:16 -0500

    accidentally left out assets

--- Makefile
+++ Makefile
@@ -93,4 +93,5 @@ release: $(BUILDDIR)/stupid_test
 .PHONY: clean
 clean:
 	rm -rf $(BUILDDIR)
+	test -f assets/shaders/*.spv && rm assets/shaders/*.spv || exit 0
 
--- shaders.sh
+++ shaders.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ./shaders
+test -d ../assets/shaders || mkdir -p ../assets/shaders
 for f in *.vert ; do glslc -fshader-stage=vert --target-env=vulkan1.4 -o ../assets/shaders/"$f".spv "$f" || exit 1 ; echo "$f" ; done
 for f in *.frag ; do glslc -fshader-stage=frag --target-env=vulkan1.4 -o ../assets/shaders/"$f".spv "$f" || exit 1 ; echo "$f" ; done
 for f in *.comp ; do glslc -fshader-stage=comp --target-env=vulkan1.4 -o ../assets/shaders/"$f".spv "$f" || exit 1 ; echo "$f" ; done
